f7b3b41056fb1c095523acc5c3fe2c2ecdbfc9fe
muhsinSutanto <sutantomuhsin@gmail.com>
2018-10-19 23:31:21 +0700

    level 14 15 16

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-//------------ level 6 ---------------
+//------------ level 6 and 7 ---------------
+// changing the first letter to capital
+// in function set the param , change the param to lowercase to make all small then split it to chage to array/
+// afterthat make looping to read the array index
+// str[i] is str[i] divided to char based on the index (firts) then change to uppercase plus str[i] and sting that is change deleted by slice
 
-let sentenceA = 'HELLO WORLD';
-let sentenceB = 'hello world';
+// let sentenceB = 'nama ku'
 
-let besar = function(str) {
-    str = sentenceB.toLowerCase().split(' ');
-    for( var i = 0; i < str.length; i++) {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-    }
+// let titlecase = function(str) {
+//     str = sentenceB.toLowerCase().split(' ');
+//     for( var i = 0; i < str.length; i++) {
+//         str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+//     }
 
-    return str.join(' ');
+//     return str.join(' ');
+// }
 
-}
+// let rubah = sentenceB.join('');
+// console.log(titlecase(sentenceB));
 
-console.log(besar(sentenceB))
+// level 14, 15, 16
+
+let add = (a, b) =>  a + b;
+let multiply = (a,b) => a * b;
+let devide = (a,b) => a / b;
+let substract = (a, b) => a - b;
+let modulo = (a,b) => a % b
+
+
+
+console.log(add(7,2));
+console.log(multiply(7,2))
+console.log(devide(7,2))
+console.log(substract(7,2))
+console.log(modulo(7,2))
+
+console.log(add(7,2), substract(4,5));
+
+
+
+
+
+
+
+
+
